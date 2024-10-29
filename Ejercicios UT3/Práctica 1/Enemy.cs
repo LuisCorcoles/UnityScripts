7349e6f801b3cd5a6b089b118c4cb9b577199ad0
LuisCor <luiscorcolesmolina@gmail.com>
2024-10-29 19:50:08 +0100

    Add files via upload

---
+++ Ejercicios UT3/Práctica 1/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Enemigo : Personaje
+{
+    public Transform jugador;
+    public float velocidadEnemigo = 3f;
+
+    void Update()
+    {
+        if (jugador != null)
+        {
+            Mover();
+        }
+    }
+
+    public override void Mover()
+    {
+        transform.LookAt(jugador.position);
+        transform.Translate(Vector3.forward * velocidadEnemigo * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision colision)
+    {
+        if (colision.gameObject.CompareTag("Jugador"))
+        {
+            HacerDaño(colision.gameObject.GetComponent<Jugador>());
+        }
+    }
+}
---
+++ Ejercicios UT3/Práctica 1/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Jugador : Personaje
+{
+    public float velocidad = 5f;
+    public int puntos = 0;
+
+    void Update()
+    {
+        Mover();
+    }
+
+    public override void Mover()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(horizontal, 0, vertical) * velocidad * Time.deltaTime;
+        transform.Translate(movimiento);
+    }
+
+    void OnTriggerEnter(Collider otro)
+    {
+        if (otro.gameObject.CompareTag("Recolectable"))
+        {
+            puntos++;
+            Destroy(otro.gameObject);
+            Debug.Log("Puntos: " + puntos);
+        }
+    }
+
+    void OnCollisionEnter(Collision colision)
+    {
+        if (colision.gameObject.CompareTag("Enemigo"))
+        {
+            HacerDaño(colision.gameObject.GetComponent<Enemigo>());
+        }
+    }
+}
